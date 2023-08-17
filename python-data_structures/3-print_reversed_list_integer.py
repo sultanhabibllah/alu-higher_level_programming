6798d2d0a84122e809afc334627b75a7343d14a2
sultanhabibllah <h.ayodele@alustudent.com>
2023-08-17 09:52:53 -0700

    Add solution for exercise 3

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        pass
+    else:
+        my_list.sort(reverse=True)
+        for i in my_list:
+            print("{:d}".format(i))
+
+print_reversed_list_integer()
