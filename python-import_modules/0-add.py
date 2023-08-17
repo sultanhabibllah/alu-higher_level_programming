851e6bf0cd43d7694a2bd60021bbcae6bbf90505
sultanhabibllah <h.ayodele@alustudent.com>
2023-08-17 13:31:15 -0700

    Fix add_0 usage issue

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
 
-def add(a, b):
-    """My addition function
+# Using add_0 function from the add_0.py module
+from add_0 import add
 
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
-
-if __name__ == "__main__":
+def main():
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    result = add(a, b)
+    print(f"{a} + {b} = {result}")
+
+if __name__ == "__main__":
+    main()
