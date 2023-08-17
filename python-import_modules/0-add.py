fe67339a945be5384ed641caee705f4b51657a19
sultanhabibllah <h.ayodele@alustudent.com>
2023-08-17 11:26:00 -0700

    Add solution for task 0: 0-add.py and add_0.py files

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,13 @@
-#!/usr/bin/python3
+# Import the add function from add_0.py
 from add_0 import add
-:x
+
+def main():
+    # Assign values to variables a and b
     a = 1
     b = 2
+
+    # Call the add function and print the result using string format
     print("{} + {} = {}".format(a, b, add(a, b)))
+
+if __name__ == "__main__":
+    main()
