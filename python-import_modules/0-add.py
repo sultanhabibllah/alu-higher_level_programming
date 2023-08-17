c0cb46320339fc53c04a641c26fd89e46a865e3e
sultanhabibllah <h.ayodele@alustudent.com>
2023-08-17 13:52:11 -0700

    another trial

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
-
-# Using add_0 function from the add_0.py module
 from add_0 import add
-
-def main():
+:x
     a = 1
     b = 2
-    result = add(a, b)
-    print("{} + {} = {}".format(a, b, result))
-
-if __name__ == "__main__":
-    main()
+    print("{} + {} = {}".format(a, b, add(a, b)))
