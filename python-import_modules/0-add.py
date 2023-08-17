f6a4ee8d7a2bbc20f542abfa872ce96118a0269b
sultanhabibllah <h.ayodele@alustudent.com>
2023-08-17 13:24:27 -0700

    Add program that imports and uses add function

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,16 +1,5 @@
-#!/usr/bin/python3
-"""This module imports the add function from add_0.py and prints the result of 1 + 2 = 3"""
-
-# Import the add function from add_0.py
+a = 1
+b = 2
 from add_0 import add
 
-def main():
-    # Assign values to variables a and b
-    a = 1
-    b = 2
-
-    # Call the add function and print the result using string format
-    print("{} + {} = {}".format(a, b, add(a, b)))
-
-if __name__ == "__main__":
-    main()
+print("{} + {} = {}".format(a, b, add(a, b)))
