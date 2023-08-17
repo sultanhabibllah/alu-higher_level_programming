ab3a084f47b231adff7963374be45dce3c9d8fc3
sultanhabibllah <h.ayodele@alustudent.com>
2023-08-17 13:29:24 -0700

    Fix execution when imported issue

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 
-a = 1
-b = 2
-from add_0 import add
+def add(a, b):
+    """My addition function
 
-print("{} + {} = {}".format(a, b, add(a, b)))
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
