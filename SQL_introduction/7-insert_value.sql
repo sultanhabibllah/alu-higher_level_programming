d303af287e2e2b43534c05d5b28bb81f793d7e61
sultanhabibllah <h.ayodele@alustudent.com>
2023-08-18 11:08:45 -0700

    fsw

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,5 @@
--- inserting into table first_table
-USE hbtn_0c_0;
+-- SQL script to insert a new row in the first_table
+-- Author: Your Name
+-- Date: August 12, 2023
+
 INSERT INTO first_table (id, name) VALUES (89, 'Best School');
